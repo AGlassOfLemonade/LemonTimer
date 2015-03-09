97053986ccb6eda1c6edcf85ed09e6747a38d207
Shawn <sdg18@duke.edu>
2015-03-08 20:07:24 -0400

    cleaning up

--- config.rb
+++ config.rb
@@ -1,5 +1,4 @@
 # Require any additional compass plugins here.
-
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
@@ -7,17 +6,12 @@ sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "js"
 fonts_dir = "fonts"
-
 output_style = :expanded
-
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
-
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
 color_output = false
-
-
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
